92e74afc5c11e1f6e49e3d82dc38f35422cca2f2
JacobBothell <jacob20140273@gmail.com>
2016-04-06 20:15:50 -0500

    Update People.h
    full implementation

--- People.h
+++ People.h
@@ -2,22 +2,40 @@
 //CS303-Project-2
 
 //Due 3-25-16
+#ifndef PEOPLE_H
+#define PEOPLE_H
+
 
 class People
 {
-private:
-	int cur_floor;	//current floor of the person
-	
-	int dir;		//direction of travel	0 - no movement
-					//						1 - down
-					//						2 - up
-	
-	int dest;		//destination floor of the person
-
 public:
-	int direction();		//need getter
+    People();
+    People(int floors);
+
+
+
+    int direction() { return dir; }         	//need getter
 	
-	int current();			//need getter
+    int current() { return cur_floor; }			//need getter
 	
-	int destination();		//need getter
+    int destination() { return dest; }          //need getter
+    
+    
+    bool operator==(const People& rhs) { return cur_floor == rhs.cur_floor; }
+    bool operator!=(const People& rhs) { return cur_floor != rhs.cur_floor; }
+    bool operator>(const People& rhs) { return cur_floor > rhs.cur_floor; }
+    bool operator<(const People& rhs) { return cur_floor < rhs.cur_floor; }
+    bool operator>=(const People& rhs) { return cur_floor >= rhs.cur_floor; }
+    bool operator<=(const People& rhs) { return cur_floor <= rhs.cur_floor; }
+
+private:
+    int cur_floor;	//current floor of the person
+
+    int dir;		//direction of travel	0 - no movement
+    //						1 - down
+    //						2 - up
+
+    int dest;		//destination floor of the person
 };
+
+#endif
