5c18a5963feaaba287d4980ed252b89712b39d2d
JacobBothell <jacob20140273@gmail.com>
2016-04-06 20:12:29 -0500

    Update E_Bank.h
    new constructor

--- E_Bank.h
+++ E_Bank.h
@@ -1,7 +1,7 @@
 //Jacob Bothell
 //CS303-Project-2
 
-//Due 3-25-16
+//Due 4-6-16
 
 #include"People.h"
 #include"Ele.h"
@@ -18,10 +18,13 @@ private:
 	list<Ele> lifts;
 
 public:
+    E_Bank();
+    E_Bank(int elevators);
+
 	void request(list<People> new_requests);	//processes requests made by People
 												//adds request to appropriate elevator
 	
-	void update();								//updates the status of elevator
+	void update(list<People>& old);								//updates the status of elevator
 												//current floor, direction, stops
 												//called by E_Bank for each elevator
 };
