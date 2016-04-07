aa86fe792937fafbcc439146153dcc2192958944
JacobBothell <jacob20140273@gmail.com>
2016-04-06 20:14:51 -0500

    Update Ele.h
    controlls

--- Ele.h
+++ Ele.h
@@ -3,29 +3,51 @@
 
 //Due 3-25-16
 
+#ifndef ELE_H
+#define ELE_H
+
 #include <vector>
+#include <list>
+#include <ostream>
+
+#include "People.h"
 
 using namespace std;
 
 class Ele
 {
 private:
-	int cur_floor;		//current floor of the person
+    int cur_floor;  //current floor of the elevator
+    int waiting;
+    int wait_len;
 	
-	int dir;			//direction of travel	0 - no movement
-						//						1 - down
-						//						2 - up
+    int dir;    //direction of travel   0 - no movement
+                                    //  1 - down
+                                    //  2 - up
 	
-	vector<int> stops;	//vector of floor stops
+    vector<int> stops;	//vector of floor stops
+    
+    void takePeople(list<People>& old);
 
 public:
-	int current();					//need getter
+    Ele();
+    
+    void dumpdebug(ostream& out);
+    
+    int current();  //need getter
 	
-	int direction();				//need getter
+    int direction();    //need getter
+    void setDirection(int d);
+    
+    int waitTime(); // >0 the door is open
+    int waitLength();
+    void setWaitLength(int w);
 	
-	void add_stop(int stop);		//need adds and sorts stop
+    void add_stop(int stop);    //need adds and sorts stop
 	
-	void update();					//updates the status of elevator
-									//current floor, direction, stops
-									//called by E_Bank for each elevator
+    void update(list<People>& old);  //updates the status of elevator
+                    //current floor, direction, stops
+                    //called by E_Bank for each elevator
 };
+
+#endif
